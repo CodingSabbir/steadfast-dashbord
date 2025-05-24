feea75f46950b7566fb5fc2dc30aa201c5ef79cf
CodingSabbir <mdsabbirrahmanm740@gmail.com>
2025-05-23 21:38:32 -0700

    pickNDrop

--- app/dashboard/add-parcel/page.js
+++ app/dashboard/add-parcel/page.js
@@ -1,23 +1,13 @@
 
+import Button from "../../components/add-parcel/Button"
 import DeliveryForm from "../../components/add-parcel/DeliveryForm"
 export default function AddParcelsPage() {
-    return (
-      <>
-   <div className="flex gap-4 mb-5 justify-end">
-  <button className="px-4 py-2 button-primary text-white rounded-sm text-md font-medium whitespace-nowrap">
-    Regular
-  </button>
-  <button className="px-4 py-2 border border-gray text-primary rounded-sm text-md font-medium whitespace-nowrap">
-    Express
-  </button>
-  <button className="px-4 py-2 border border-gray text-primary rounded-sm text-md font-medium whitespace-nowrap">
-    PickNDrop
-  </button>
-</div>
-
+  return (
+    <>
+      <Button />
       <div className="min-h-screen bg-gray-50 p-4">
-      <DeliveryForm />
-    </div>
-      </>
-    )
+        <DeliveryForm />
+      </div>
+    </>
+  )
 }
